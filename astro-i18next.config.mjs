212f4bd470030a4ae39e0a21ea65522208885b8c
かりこ <webcreate0303yokoi0630@gmail.com>
2024-05-12 20:29:58 +0900

    delete :wastebasket: #2 astro-i18nextの削除 - t関数でpublic内の翻訳情報を読み取れない - 標準機能で対応可能であると判断

--- astro-i18next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('astro-i18next').AstroI18nextConfig} */
-export default {
-  defaultLocale: "ja",
-  locales: ["ja", "en"],
-  namespaces: ["common"],
-  defaultNamespace: "common",
-};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
-// ---- 【統合】i18n / 多言語対応 ----
-import astroI18next from "astro-i18next";
 // ---- 【統合】lit / WEB Conponents ----
 import lit from "@astrojs/lit";
 
 export default defineConfig({
   i18n: {
     defaultLocale: "ja",
-    locales: ["ja", "en"]
+    locales: ["ja", "en"],
   },
-  integrations: [astroI18next(), lit()]
+  integrations: [lit()]
 });
