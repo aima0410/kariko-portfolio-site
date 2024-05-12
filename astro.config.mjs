69c780662607545100ce600e917b69655d11e32e
かりこ <webcreate0303yokoi0630@gmail.com>
2024-05-13 06:40:40 +0900

    dep :package: #2 sitmap,robots,partytown,mdx 追加と設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 import { defineConfig } from "astro/config";
 // ---- 【統合】lit / WEB Conponents ----
 import lit from "@astrojs/lit";
+// ---- 【統合】sitemap / サイトマップ自動生成 ----
+import sitemap from "@astrojs/sitemap";
+// ---- 【統合】robots-txt / robots.txt自動生成 ----
+import robotsTxt from "astro-robots-txt";
+// ---- 【統合】partytown / サードパーティ対策 ----
+import partytown from "@astrojs/partytown";
+// ---- 【統合】MDX / マークダウン拡張機能導入 ----
+import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
   i18n: {
     defaultLocale: "ja",
-    locales: ["ja", "en"],
+    locales: ["ja", "en"]
   },
-  integrations: [lit()]
+  site: "http://localhost:4321/",
+  integrations: [lit(), sitemap(), robotsTxt(), partytown(), mdx()]
 });
